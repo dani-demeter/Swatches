4d4bf1d592b416530d5c0fd97bf9fdc43a968abb
Dani Demeter <flamegiraffes@gmail.com>
2019-05-16 19:16:48 -0700

    color changing tab

--- main.js
+++ main.js
@@ -9,39 +9,49 @@ var topBrd = 50;
 var leftBrd = 50;
 var rightBrd = 50;
 var botBrd = 50;
+var squishedRightBrd = 250;
+var originalRightBrd = 50;
 
 var selectedSquare = -1;
+var selectedSquareTab;
 function setup(){
    w = window.innerWidth;
    h = window.innerHeight;
    cnv = createCanvas(w, h);
    cnv.position(0,0);
    cnv.background(color('#090821'));
-   AddNewColorHex("F5F0F6");
-   AddNewColorHex("2AB7CA");
-   AddNewColorHex("241E4E");
-   AddNewColorHex("9E0031");
-   AddNewColorHex("FAA916");
-   AddNewColorHex("662AAF");
-   AddNewColorHex("330036");
-   AddNewColorHex("FFC700");
-   AddNewColorHex("2C363F");
-   AddNewColorHex("8FB2CE");
-   AddNewColorHex("E5F8FF");
-   AddNewColorHex("007BA7");
+
+   AddNewColorHex("whitesmoke", "F5F0F6");
+   AddNewColorHex("ballblue", "2AB7CA");
+   AddNewColorHex("spacecadet", "241E4E");
+   AddNewColorHex("raspberry", "9E0031");
+   AddNewColorHex("tangerine", "FAA916");
+   AddNewColorHex("grape-light", "662AAF");
+   AddNewColorHex("purple-dark", "330036");
+   AddNewColorHex("gold", "FFC700");
+   AddNewColorHex("gunmetal", "2C363F");
+   AddNewColorHex("ceil", "8FB2CE");
+   AddNewColorHex("bubbles", "E5F8FF");
+   AddNewColorHex("cerulean", "007BA7");
+
+   selectedSquareTab = createDiv('');
+   selectedSquareTab.position(w, topBrd);
+   selectedSquareTab.size(squishedRightBrd-2*gap-originalRightBrd, h-topBrd-botBrd);
+   selectedSquareTab.class('selectedSquareTab');
 }
 
-function AddNewColorRGB(r, g, b){
+function AddNewColorRGB(name, r, g, b){
    // nsq = createDiv(`${r}, ${g}, ${b}`);
    var hex = rgb2Hex(r, g, b).toUpperCase();
-   AddNewColorHex(hex);
+   AddNewColorHex(name,hex);
 }
 
-function AddNewColorHex(hex){
+function AddNewColorHex(name,hex){
    let nsq = createDiv('');
    hex = hex.toUpperCase();
    var c = color("#"+hex);
-   let nsqText = createDiv(`${red(c)}, ${green(c)}, ${blue(c)} <br /> #${hex}`);
+   // let nsqText = createDiv(`${red(c)}, ${green(c)}, ${blue(c)} <br /> #${hex}`);
+   let nsqText = createDiv(name);
    nsq.class('colorSquare');
    nsqText.class('colorSquareText');
    nsq.child(nsqText);
@@ -49,19 +59,27 @@ function AddNewColorHex(hex){
    let myInd = myColors.length;
    nsq.position(myInd%gx*((w-leftBrd-rightBrd)/gx)+gap/2+leftBrd, Math.floor(myInd/gx)*(((h-topBrd-botBrd)/gy))+gap/2+topBrd);
    nsq.style('background-color', c);
-   // nsqText.style('color', c);
 
    nsq.mousePressed(()=>{
-      rightBrd = 250;
+      rightBrd = squishedRightBrd;
       if(selectedSquare==myInd){
          selectedSquare = -1;
-         rightBrd = 50;
+         rightBrd = originalRightBrd;
+         nsq.removeClass('selectedSquare');
+         selectedSquareTab.position(w, topBrd);
       }else{
+         if(selectedSquare!=-1){
+            myColors[selectedSquare].square.removeClass('selectedSquare');
+         }
+         selectedSquareTab.position(w+gap-squishedRightBrd, topBrd);
          selectedSquare = myInd;
+         selectedSquareTab.style('background-color', color(myColors[myInd].r,myColors[myInd].g,myColors[myInd].b) );
+         nsq.addClass('selectedSquare');
       }
       redrawSquares();
    });
    myColors.push({
+      name: name,
       r: red(c),
       g: green(c),
       b: blue(c),
